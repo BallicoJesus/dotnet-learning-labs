d27d926f871ff7868b2b69bd72ce225e40a43750
BallicoJesus <Ballico17@gmail.com>
2026-07-20 00:45:22 -0300

    Inicializar Variables

---
+++ HelloDotNet/01 - Bases/Variables.cs
@@ -0,0 +1,11 @@
+partial class Program
+{
+    static void Variables()
+    {
+        int number = 25;
+        string nombre = "Alice";
+        bool esEstudiante = true;
+        Console.WriteLine($"Número: {number}, Nombre: {nombre}, Es estudiante: {esEstudiante}");
+    }
+       
+    }
